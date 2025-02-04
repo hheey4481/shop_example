d732c24b93478270a7dd66047e7fcf1c000b230d
hheey <hheey4481@gmail.com>
2025-02-04 09:19:24 +0900

    feat: add category at Product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,11 +3,13 @@ class Product {
   final String name;
   final double price;
   final String imageUrl;
+  final String category;
 
   Product({
     required this.id,
     required this.name,
     required this.price,
     required this.imageUrl,
+    required this.category,
   });
 }
